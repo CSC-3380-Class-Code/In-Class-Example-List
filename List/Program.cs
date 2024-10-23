1cce4043b29ecac9ed6532739d080caab70d3330
John Luke <jldenny0207@aol.com>
2024-10-23 01:20:20 -0500

    TEsted Example and added README

---
+++ List/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+public class List<Type> where Type : IComparable{
+	public Type[] list;
+	private int size;
+
+	public List(){
+		list = [];
+		size = 0;
+	}
+
+	public List(Type[] arr){
+		list = arr;
+		size = arr.Length;
+	}
+
+	public static List<Type> operator +(List<Type> l, Type elem){
+		int return_size = 1+l.size;
+		Type[] return_list = new Type[return_size];
+
+		for(int i = 0; i < l.size; i++){
+			return_list[i] = l.list[i];
+		}
+		return_list[return_size-1] = elem;
+		return new List<Type>(return_list);
+	}
+
+	public static List<Type> operator +(List<Type> l1, List<Type> l2){
+		foreach(Type val in l2.list){
+			l1 += val;
+		}
+		return l1;
+	}
+
+	public int Index(){
+		return list.Length;
+	}
+
+	public bool Sort(){
+		for(int i = 0; i < list.Length-1; i++){
+			for(int j = i+1; j < list.Length; j++){
+				if(list[i].CompareTo(list[j]) > -1){
+					Type temp = list[i];
+					list[i] = list[j];
+					list[j] = temp;
+				}
+			}
+		}
+
+		return true;
+	}
+
+    public override string ToString()
+    {
+		string return_string = "[";
+		foreach(Type val in list){
+			return_string += $"{val},";
+		}
+
+		return_string += "]";
+
+        return return_string;
+    }
+
+
+
+}
+
+public class ListTest{
+	public static void Main(){
+		int[] arr = {0,2,1,3};
+		int elem = 5;
+
+		List<int> l = new List<int>(arr);
+		Console.WriteLine(l);
+
+		l = l + elem;
+		Console.WriteLine(l);
+
+		l = l + l;
+		Console.WriteLine(l);
+
+		l.Sort();
+		Console.WriteLine(l);
+
+		string[] str_arr = {"1", "Hello"};
+		string str1 = "HI";
+
+		List<string> l2 = new List<string>(str_arr);
+		Console.WriteLine(l2);
+
+		l2 = l2 + str1;
+		Console.WriteLine(l2);
+
+		l2 = l2 + l2;
+		Console.WriteLine(l2);
+
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # In-Class-Example-List
+
+This is the Generic List made in class.
+To see how to fully use Generics in C#, check <a href="https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/generics/generic-classes">here</a>.
+
+# Running commands
+
+Make sure your current directory in terminal is Promises. You can check this with the following command:
+```bash
+# windows
+cd
+
+# Mac or Linux
+pwd
+```
+
+After running the command, you should see a path similar to the following:
+```
+[Full path]/In-Class-Example-List/List
+```
+
+To look at the file, open Program.cs in the List folder
+
+To run the file, use the following command to see the output:
+
+```bash
+dotnet run
+```
